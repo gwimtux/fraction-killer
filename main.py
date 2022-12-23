1724ddd8211b495f91cb5e6dab646ede5f12a023
gwimtux <g5l@protonmail.com>
2022-12-22 21:29:51 -0800

    added more features

--- main.py
+++ main.py
@@ -1,21 +1,20 @@
-import os
+#!/usr/bin/env python3
+import argparse
 
+
+parser = argparse.ArgumentParser(
+                            prog='fractionkiller',
+                            description='add and subtract fractions without reducing!',
+                            epilog='made by gwim')
+parser.add_argument('-l', '--add', action='store_true', help='Add two fractions')
+parser.add_argument('-s', '--sub', action='store_true', help='Subtract two fractions')
+args = parser.parse_args()
 
 
 class style():
     BOLD = '\033[1m'
     END = '\033[0m'
 
-def main_logo():
-    print(style.BOLD + '''
-    @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-    @@ FRACTION KILLER (by gwim) @@
-    @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-    ''' + style.END + '''         
-              [l] - Add
-              [s] - Subtract''')
-
-os.system('clear')
 def add(a, b, c, d):
     numerator = a + c
     #if numerator > 32:
@@ -24,7 +23,6 @@ def add(a, b, c, d):
     denominator = 32
     return print(f'{numerator}/{denominator}')
 
-os.system('clear')
 def sub(a, b, c, d):
     numerator = a - c
     if numerator < 0:
@@ -33,20 +31,19 @@ def sub(a, b, c, d):
     denominator = 32
     return print(f'{numerator}/{denominator}')
     
-os.system('clear')
 def check_input():
-    main_logo()
-    print('''
-    ''')
-    i = input('> ')
-    if i == 'l':
+    if args.add:
         check_var_add()
-    elif i == 's':
-        return check_var_sub()
+    elif args.sub:
+        check_var_sub()
+    else:
+        print('Error: No arguments')
+        exit()
+
 
 def check_var_add():
-    a = str(input('1: '))    
-    b = str(input('2: '))   
+    a = str(input('$: '))    
+    b = str(input('$: '))   
     a = a.split('/')[0]
     b = b.split('/')[0] 
     a = int(a)
@@ -54,10 +51,11 @@ def check_var_add():
     return add(a, 32, b, 32)
 
 def check_var_sub():
-    a = str(input('1: '))    
-    b = str(input('2: '))   
+    a = str(input('a: '))    
+    print('/')
+    b = str(input('b: '))
     a = a.split('/')[0]
-    b = b.split('/')[0] 
+    b = a.split('/')[0] 
     a = int(a)
     b = int(b)
     return sub(a, 32, b, 32)
